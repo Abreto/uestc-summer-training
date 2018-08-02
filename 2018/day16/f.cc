5c5d4dc4147f2cd3128d3e6cdcca08994a572c5c
Abreto Fu <m@abreto.net>
2018-08-03 02:04:25 +0800

    resubmit

---
+++ 2018/day16/f.cc
@@ -0,0 +1,243 @@
+#include <cstdio>
+#include <algorithm>
+
+using namespace std;
+
+class abio
+{
+    static const unsigned BUF_SZ = 65536;
+    FILE *istream, *ostream;
+    char ibuf[BUF_SZ], obuf[BUF_SZ];
+    bool reached_eof;
+    size_t ip, isz;
+    size_t op, osz;
+    inline void clear_ibuf(void) { ip = isz = 0u; }
+    inline void clear_obuf(void) { op = osz = 0u; }
+    inline void clear_buffer(void)
+    {
+        reached_eof = false;
+        clear_ibuf();
+        clear_obuf();
+    }
+    inline size_t read_buffer(void)
+    {
+        isz = fread(ibuf, sizeof(char), BUF_SZ, istream);
+        ip = 0;
+        return isz;
+    }
+    inline size_t write_buffer(void)
+    {
+        if(osz)
+        {
+            size_t ret = fwrite(obuf+op, sizeof(char), osz-op, ostream);
+            op += ret;
+            if(op == osz) clear_obuf();
+            return ret;
+        }
+        return 0;
+    }
+    inline abio &reach_eof(void)
+    {
+        reached_eof = true;
+        return (*this);
+    }
+public:
+    static const char endl = '\n';
+    abio(FILE *input = stdin, FILE *output = stdout)
+    {
+        this->istream = input;
+        this->ostream = output;
+        clear_buffer();
+    }
+    abio(const char *input, const char *output)
+    {
+        this->istream = fopen(input, "r");
+        this->istream = fopen(output, "w+");
+        clear_buffer();
+    }
+    ~abio(void)
+    {
+        write_buffer();
+        fclose(istream);
+        fclose(ostream);
+    }
+    operator bool() const
+    {
+        return (!reached_eof);
+    }
+    inline int getchar(void)
+    {
+        if(0 == isz || isz == ip) read_buffer();
+        if(0 == isz || isz == ip) return EOF;
+        return ibuf[ip++];
+    }
+    inline int putchar(int ch)
+    {
+        if(osz == BUF_SZ) write_buffer();
+        if(osz == BUF_SZ) return EOF;
+        return (obuf[osz++] = ch);
+    }
+    abio &read_int(int &x)
+    {
+        int flag = 0, ch = getchar();
+        if(EOF == ch) return (this->reach_eof());
+        x = 0;
+        while((EOF!=ch)&&(('-'!=ch)&&((ch<'0')||(ch>'9'))))ch=getchar();
+        if(EOF==ch) return (this->reach_eof());
+        if('-'==ch){flag=1;ch=getchar();}
+        //if(EOF==ch) return (this->reach_eof());
+        for(;(((ch>='0')&&(ch<='9')));ch=getchar()){x*=10;x+=(ch-'0');}
+        //if(EOF==ch)this->reach_eof();
+        if(flag)x*=(-1);
+        return (*this);
+    }
+    abio &read_ll(long long int &x)
+    {
+        int flag = 0, ch = getchar();
+        if(EOF == ch) return (this->reach_eof());
+        x = 0ll;
+        while((EOF!=ch)&&(('-'!=ch)&&((ch<'0')||(ch>'9'))))ch=getchar();
+        if(EOF==ch) return (this->reach_eof());
+        if('-'==ch){flag=1;ch=getchar();}
+        //if(EOF==ch) return (this->reach_eof());
+        for(;(((ch>='0')&&(ch<='9')));ch=getchar()){x*=(10ll);x+=(ch-'0');}
+        //if(EOF==ch)this->reach_eof();
+        if(flag)x*=(-1);
+        return (*this);
+    }
+    /* set interrupt as '\n' to read a whole line. */
+    abio &read_s(char *s, const char interrupt = ' ')
+    {
+        int ch = getchar();
+        while((EOF!=ch)&&(ch<'!'||ch>'~'))ch=getchar();
+        if(EOF==ch) return (this->reach_eof());
+        for(;(EOF!=ch)&&(interrupt!=ch)&&(ch>=' '&&ch<='~');ch=getchar())(*s++)=ch;
+        (*s)=0;
+        return (*this);
+    }
+    abio &write_int(int x, char append = 0)
+    {
+        int d[20],nd=0;
+        if(0==x) putchar('0');
+        if(x<0){putchar('-');x=-x;}
+        while(x){d[nd++]=x%10;x/=10;}
+        while(nd--)putchar('0'+d[nd]);
+        if(append)putchar(append);
+        return (*this);
+    }
+    abio &write_ll(long long int x, char append = 0)
+    {
+        int d[20],nd=0;
+        if(0==x) putchar('0');
+        if(x<0){putchar('-');x=-x;}
+        while(x){d[nd++]=x%10;x/=10;}
+        while(nd--)putchar('0'+d[nd]);
+        if(append)putchar(append);
+        return (*this);
+    }
+    abio &write_s(const char *s, char append = 0)
+    {
+        while(*s) putchar(*s++);
+        if(append) putchar(append);
+        return (*this);
+    }
+    abio &operator>>(char &ch)
+    {
+        ch = getchar();
+        if(EOF==ch) return (this->reach_eof());
+        return (*this);
+    }
+    abio &operator>>(int &x) { return read_int(x); }
+    abio &operator>>(long long int &x) { return read_ll(x); }
+    abio &operator>>(char *s) { return read_s(s); }
+    abio &operator<<(const char ch) { putchar(ch); return (*this); }
+    abio &operator<<(const int x) { return write_int(x); }
+    abio &operator<<(const long long int x) { return write_ll(x); }
+    abio &operator<<(const char *s) { return write_s(s); }
+}io(fopen("f.in","r"));
+
+class acmachine
+{
+    typedef long long int ll;
+    static const ll mod = 998244353;
+    static const int maxn = 100010;
+    ll qow(ll a, ll p)
+    {
+        ll ret = 1;
+        while(p)
+        {
+            if(1&p) ret = (ret * a) % mod;
+            a = (a * a) % mod;
+            p >>= 1;
+        }
+        return ret;
+    }
+    int n;
+    ll prod[maxn];
+    int nd, ds[maxn];
+    inline int lowbit(int x) { return (x)&(-x); }
+    void init(void)
+    {
+        for(int i = 1;i <= nd;i++)
+        {
+            prod[i] = 1ll;
+        }
+    }
+    void open(int i, ll p)
+    {
+        while(i <= nd)
+        {
+            prod[i] = (prod[i] * p) % mod;
+            i += lowbit(i);
+        }
+    }
+    ll get(int i)
+    {
+        ll ret = 1ll;
+        while(i > 0)
+        {
+            ret = (ret * prod[i]) % mod;
+            i -= lowbit(i);
+        }
+        return ret;
+    }
+    void discreate(void)
+    {
+        sort(ds, ds+nd);
+        nd = unique(ds, ds+nd) - ds;
+    }
+    int id(int di)
+    {
+        return lower_bound(ds, ds+nd, di) - ds + 1;
+    }
+    int p[maxn], d[maxn];
+public:
+    int wa()
+    {
+        int i;
+        io>>n;
+        for(i = 0;i < n;i++)
+        {
+            io>>p[i]>>d[i];
+            ds[nd++] = d[i];
+        }
+        discreate(); init();
+        ll ans = 0ll;
+        ll inv100 = qow(100ll, mod-2);
+        for(i = 0;i < n;i++)
+        {
+            int did = id(d[i]);
+            ll delta = get(nd+1-did);
+            delta = (((delta * p[i]) % mod) * inv100) % mod;
+            ans = (ans + delta) % mod;
+            open(nd+1-did, ((100ll-p[i]) * inv100) % mod);
+        }
+        io<<ans<<io.endl;
+        return 0;
+    }
+};
+
+int main(void)
+{
+    return ((new acmachine())->wa());
+}
