cccb317528cb18531c3e9b7e44a880d92a49488b
Abreto Fu <m@abreto.net>
2018-08-16 01:00:35 +0800

    buti

---
+++ 2018/day24/11.cc
@@ -0,0 +1,299 @@
+#include <cstdio>
+
+class abio
+{
+    static const unsigned BUF_SZ = 65536;
+    FILE *istream, *ostream;
+    char ibuf[BUF_SZ], obuf[BUF_SZ];
+    bool reached_eof;
+    size_t ip, isz;
+    size_t op, osz;
+    inline void clear_ibuf(void) { ip = isz = 0u; }
+    inline void clear_obuf(void) { op = osz = 0u; }
+    inline void clear_buffer(void)
+    {
+        reached_eof = false;
+        clear_ibuf();
+        clear_obuf();
+    }
+    inline size_t read_buffer(void)
+    {
+        isz = std::fread(ibuf, sizeof(char), BUF_SZ, istream);
+        ip = 0;
+        return isz;
+    }
+    inline size_t write_buffer(void)
+    {
+        if(osz)
+        {
+            size_t ret = std::fwrite(obuf+op, sizeof(char), osz-op, ostream);
+            op += ret;
+            if(op == osz) clear_obuf();
+            return ret;
+        }
+        return 0;
+    }
+    inline abio &reach_eof(void)
+    {
+        reached_eof = true;
+        return (*this);
+    }
+public:
+    static const char endl = '\n';
+    abio(FILE *input = stdin, FILE *output = stdout)
+    {
+        this->istream = input;
+        this->ostream = output;
+        clear_buffer();
+    }
+    abio(const char *input, const char *output)
+    {
+        this->istream = std::fopen(input, "r");
+        this->istream = std::fopen(output, "w+");
+        clear_buffer();
+    }
+    ~abio(void)
+    {
+        write_buffer();
+        std::fclose(istream);
+        std::fclose(ostream);
+    }
+    operator bool() const
+    {
+        return (!reached_eof);
+    }
+    inline int getchar(void)
+    {
+        if(isz == ip) read_buffer();
+        if(isz == ip) return EOF;
+        return ibuf[ip++];
+    }
+    inline int putchar(int ch)
+    {
+        if(osz == BUF_SZ) write_buffer();
+        if(osz == BUF_SZ) return EOF;
+        return (obuf[osz++] = ch);
+    }
+    abio &read_int(int &x)
+    {
+        int flag = 0, ch = getchar();
+        for (;(EOF!=ch)&&((ch<'0')||(ch>'9'));ch=getchar()) if ('-' == ch) flag = 1;
+        if (EOF == ch) return (this->reach_eof()); x = 0;
+        for (;(ch>='0')&&(ch<='9');ch=getchar()) x = x * 10 + (ch - '0');
+        if ( flag ) x *= (-1);
+        return (*this);
+    }
+    abio &read_ll(long long int &x)
+    {
+        int flag = 0, ch = getchar();
+        for (;(EOF!=ch)&&((ch<'0')||(ch>'9'));ch=getchar()) if ('-' == ch) flag = 1;
+        if (EOF == ch) return (this->reach_eof()); x = 0ll;
+        for (;(ch>='0')&&(ch<='9');ch=getchar()) x = x * 10ll + (ch - '0');
+        if ( flag ) x *= (-1ll);
+        return (*this);
+    }
+    abio &read_unsigned(unsigned &x)
+    {
+        int ch = getchar();
+        for(;(EOF != ch) && ((ch < '0') || (ch > '9'));ch = getchar());
+        if (EOF == ch) return (this->reach_eof()); x = 0u;
+        for(;(ch >= '0') && (ch <= '9');ch = getchar()) x = x * 10u + (ch - '0');
+        return (*this);
+    }
+    abio &read_ull(unsigned long long int &x)
+    {
+        int ch = getchar();
+        for(;(EOF != ch) && ((ch < '0') || (ch > '9'));ch = getchar());
+        if (EOF == ch) return (this->reach_eof()); x = 0ull;
+        for(;(ch >= '0') && (ch <= '9');ch = getchar()) x = x * 10ull + (ch - '0');
+        return (*this);
+    }
+    /* set interrupt as '\n' to read a whole line. */
+    abio &read_s(char *s, const char interrupt = ' ')
+    {
+        int ch = getchar();
+        while((EOF!=ch)&&(ch<'!'||ch>'~'))ch=getchar();
+        if(EOF==ch) return (this->reach_eof());
+        for(;(EOF!=ch)&&(interrupt!=ch)&&(ch>=' '&&ch<='~');ch=getchar())(*s++)=ch;
+        (*s)=0;
+        return (*this);
+    }
+    abio &write_int(int x, char append = 0)
+    {
+        int d[20],nd=0;
+        if(0==x) putchar('0');
+        if(x<0){putchar('-');x=-x;}
+        while(x){d[nd++]=x%10;x/=10;}
+        while(nd--)putchar('0'+d[nd]);
+        if(append)putchar(append);
+        return (*this);
+    }
+    abio &write_ll(long long int x, char append = 0)
+    {
+        int d[20],nd=0;
+        if(0==x) putchar('0');
+        if(x<0){putchar('-');x=-x;}
+        while(x){d[nd++]=x%10;x/=10;}
+        while(nd--)putchar('0'+d[nd]);
+        if(append)putchar(append);
+        return (*this);
+    }
+    abio &write_unsigned(unsigned x, char append = 0)
+    {
+        int d[20],nd=0;
+        if(0==x) putchar('0');
+        while(x){d[nd++]=x%10;x/=10;}
+        while(nd--)putchar('0'+d[nd]);
+        if(append)putchar(append);
+        return (*this);
+    }
+    abio &write_ull(unsigned long long int x, char append = 0)
+    {
+        int d[20],nd=0;
+        if(0==x) putchar('0');
+        while(x){d[nd++]=x%10;x/=10;}
+        while(nd--)putchar('0'+d[nd]);
+        if(append)putchar(append);
+        return (*this);
+    }
+    abio &write_128(__int128 x, char append = 0)
+    {
+        int d[40],nd=0;
+        if(0==x) putchar('0');
+        while(x){d[nd++]=x%10;x/=10;}
+        while(nd--)putchar('0'+d[nd]);
+        if(append)putchar(append);
+        return (*this);
+    }
+    abio &write_s(const char *s, char append = 0)
+    {
+        while(*s) putchar(*s++);
+        if(append) putchar(append);
+        return (*this);
+    }
+    abio &operator>>(char &ch)
+    {
+        ch = getchar();
+        if(EOF==ch) return (this->reach_eof());
+        return (*this);
+    }
+    abio &operator>>(int &x) { return read_int(x); }
+    abio &operator>>(long long int &x) { return read_ll(x); }
+    abio &operator>>(unsigned &x) { return read_unsigned(x); }
+    abio &operator>>(unsigned long long int &x) { return read_ull(x); }
+    abio &operator>>(char *s) { return read_s(s); }
+    abio &operator<<(const char ch) { putchar(ch); return (*this); }
+    abio &operator<<(const int x) { return write_int(x); }
+    abio &operator<<(const long long int x) { return write_ll(x); }
+    abio &operator<<(const unsigned x) { return write_unsigned(x); }
+    abio &operator<<(const unsigned long long int x) { return write_ull(x); }
+    abio &operator<<(const char *s) { return write_s(s); }
+    abio &operator<<(const __int128 x) { return write_128(x); }
+}io;
+
+typedef __int128 abint;
+typedef long long int ll;
+
+class ac_machine
+{
+    static const int maxm = 16, maxn = 32;
+    ll fact[maxn];
+    int m, n, k;
+    char gird[maxm][maxn];
+    int dots[maxn], balls[maxn];
+    ll dp[maxn][1 << maxm];
+    ll ans[maxn];
+    inline int count(int x)
+    {
+        int ret;
+        for (ret = 0 ; x ; x &= (x - 1)) ret++;
+        return ret;
+    }
+    void input(void)
+    {
+        io >> m >> n >> k;
+        for (int i = 0;i < m;i++)
+        {
+            io >> gird[i];
+        }
+        for (int i = 0;i < n;i++)
+        {
+            dots[i] = balls[i] = 0;
+            for (int j = 0;j < m;j++)
+            {
+                dots[i] = (dots[i] << 1) | ('.' == gird[j][i]);
+                balls[i] = (balls[i] << 1) | ('Q' == gird[j][i]);
+            }
+        }
+    }
+    void calculate(void)
+    {
+        for (int i = 1;i <= k;i++) ans[i] = 0ll;
+        for (int pierced = 1;pierced < (1 << m);pierced++)
+        {
+            dp[0][0] = 1;
+            for (int i = 0;i < n;i++)
+            {
+                bool used = false;
+                for (int s = pierced ; ; s = (s - 1) & pierced)
+                {
+                    if ( dp[i][s] )
+                    {
+                        if ( (balls[i] & pierced) == balls[i] )
+                        {
+                            dp[i + 1][s] += dp[i][s];
+                            used = true;
+                        }
+                        for (int prick = (pierced ^ s) & balls[i] ; prick ; prick &= (prick - 1))
+                        {
+                            int balloon = prick & (-prick);
+                            dp[i + 1][s | balloon] += dp[i][s];
+                            used = true;
+                        }
+                        dp[i][s] = 0;
+                    }
+                    if (0 == s) break;
+                }
+                if (used == false) break;
+            }
+            ans[ count(pierced) ] += dp[n][pierced];
+            for (int s = pierced ; ; s = (s - 1) & pierced)
+            {
+                dp[n][s] = 0;
+                if (0 == s) break;
+            }
+        }
+    }
+    void output(void)
+    {
+        for (int i = 1;i <= k;i++)
+        {
+            abint x = ans[i], y = fact[i];
+            io << (x * y) << io.endl;
+        }
+    }
+    void tle(void)
+    {
+        input();
+        calculate();
+        output();
+    }
+public:
+    ac_machine(void)
+    {
+        fact[0] = 1ll;
+        for (ll i = 1;i <= 20;i++) fact[i] = fact[i-1] * i;
+    }
+    int wa(void)
+    {
+        int T;
+        io >> T;
+        while (T--) tle();
+        return 0;
+    }
+};
+
+int main(void)
+{
+    return (new ac_machine())->wa();
+}
